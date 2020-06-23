1104d0b0ec0e478004184d9518a572bbf9e078d1
dhuvaraggnajithraj <34213472+Dhuvaraggn@users.noreply.github.com>
2020-06-23 21:23:54 +0530

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-df=pd.read_csv("~/Documents/newmovies.csv",index_col="imdb_title_id")
+df=pd.read_csv("newmovies.csv",index_col="imdb_title_id")
 df 
 
 lg=df["language"]
